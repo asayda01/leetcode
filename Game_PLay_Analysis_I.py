143457fcabb36999f0c509563b8f26f524343a58
asayda01 <ahmetcemsaydam@gmail.com>
2024-10-24 13:04:25 +0100

    added The Game Play Analysis - 1 Challenge and its solution in SQL .
    added The Game Play Analysis - 1 Challenge and its solution in SQL .

---
+++ Game_PLay_Analysis_I.py
@@ -0,0 +1,71 @@
+"""
+
+Table: Activity
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key (combination of columns with unique values) of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+
+
+
+Write a solution to find the first login date for each player.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Activity table:
++-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-05-02 | 6            |
+| 2         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-02 | 0            |
+| 3         | 4         | 2018-07-03 | 5            |
++-----------+-----------+------------+--------------+
+Output:
++-----------+-------------+
+| player_id | first_login |
++-----------+-------------+
+| 1         | 2016-03-01  |
+| 2         | 2017-06-25  |
+| 3         | 2016-03-02  |
++-----------+-------------+
+
+"""
+
+import pandas as pd
+
+
+def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    return activity.groupby('player_id', as_index=False).agg(first_login=('event_date', 'min'))
+
+
+# ------------------------------------------
+
+import pandas as pd
+
+
+def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+
+    # Group the activity data by 'player_id' and find the minimum 'event_date' for each player
+    first_login_df = activity.groupby('player_id')['event_date'].agg(min).reset_index()
+
+    # Rename the resulting column to 'first_login'
+    first_login_df = first_login_df.rename(columns={'event_date': 'first_login'})
+
+    return first_login_df
