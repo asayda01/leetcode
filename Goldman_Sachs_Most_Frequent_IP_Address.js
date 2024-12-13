da83ec88a54b9e9b3e2c2fa4c7346b88ee1b138a
asayda01 <ahmetcemsaydam@gmail.com>
2024-12-13 16:46:11 +0000

    added the Goldman Sach`s Most Frequent IP Address Challenge and its solution in JavaScript .
    added the Goldman Sach`s Most Frequent IP Address Challenge and its solution in JavaScript .

---
+++ Goldman_Sachs_Most_Frequent_IP_Address.js
@@ -0,0 +1,37 @@
+// Given a list of logs with IP addresses in the following format.
+// lines = ["10.0.0.1 - GET 2020-08-24", "10.0.0.1 - GET 2020-08-24", "10.0.0.2 - GET 2020-08-20"]
+//
+// Return the most frequent IP address from the logs. The retuned IP address value must be in a string format.
+//  If multiple IP addresses have the count equal to max count,
+//  then return the address as a comma-separated string with IP addresses in sorted order.
+
+
+function frequentIP(addresses) {
+    let maxCount = 0;
+    const table = {};
+
+    for (const addr of addresses) {
+        const ip = addr.split(" ")[0];
+        table[ip] = (table[ip] || 0) + 1;
+        maxCount = Math.max(maxCount, table[ip]);
+    }
+
+    const maxIps = Object.entries(table)
+        .filter(([ip, count]) => count === maxCount)
+        .map(([ip]) => ip);
+
+    if (maxIps.length > 1) {
+        return maxIps.sort().join(",");
+    } else {
+        return maxIps[0];
+    }
+}
+
+// Test case
+const lines = [
+    "10.0.0.1 - GET 2020-08-24",
+    "10.0.0.1 - GET 2020-08-24",
+    "10.0.0.2 - GET 2020-08-20",
+];
+
+console.log(frequentIP(lines)); // Output: "10.0.0.1"
