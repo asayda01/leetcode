5301c2764d0e5cf5c77510686b403467e2a25707
asayda01 <ahmetcemsaydam@gmail.com>
2025-03-09 10:46:26 +0000

    added The Valid Palindrome Challenge and its solution in Java .
    added The Valid Palindrome Challenge and its solution in Java .

---
+++ Valid_Palindrome_II.java
@@ -0,0 +1,53 @@
+/*
+
+680. Valid Palindrome II
+Solved
+Easy
+Topics
+Companies
+
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+
+Example 3:
+
+Input: s = "abc"
+Output: false
+
+
+
+Constraints:
+
+    1 <= s.length <= 105
+    s consists of lowercase English letters.
+
+*/
+
+class Solution {
+
+    public boolean validPalindrome(String s) {
+        int l = -1, r = s.length();
+        while (++l < --r)
+            if (s.charAt(l) != s.charAt(r)) return isPalindromic(s, l, r+1) || isPalindromic(s, l-1, r);
+        return true;
+    };
+
+    public boolean isPalindromic(String s, int l, int r) {
+        while (++l < --r)
+            if (s.charAt(l) != s.charAt(r)) return false;
+        return true;
+    };
+
+};
